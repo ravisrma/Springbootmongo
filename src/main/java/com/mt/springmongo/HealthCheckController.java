c7dc77c94a2d661b1f96261a5421077a2fb8c646
Ravi Sharma <ravisharma0527@gmail.com>
2024-11-15 10:10:07 +0530

    first

---
+++ src/main/java/com/mt/springmongo/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.mt.springmongo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "OK";
+    }
+}
